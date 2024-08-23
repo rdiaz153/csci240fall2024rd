5e995b22cf5b78e2cefabf6e24af98b6457fc9bd
Ricardo Diaz <rdiaz153@student.mtsac.edu>
2024-08-22 23:22:22 -0700

    8/22/24

--- exercise1PA1.cpp
+++ exercise1PA1.cpp
@@ -10,9 +10,31 @@
 
 using namespace std;
 
+int duplicateValues (int a[], int size) {
+  int count = 0;
+
+  for (int i = 0; i < size; i++)
+  {
+      for (int j = i + 1; j < size; j++) 
+      {
+          if (a[i] == a[j])
+          count++;
+      }
+  }
+  return count;
+}
+
 int main()
 {
     cout << "Author: Ricardo Diaz" << endl;
+
+    int testSize1[] = {5, 7, 2, 6, 4};
+    int testSize2[] = {5, 7, 2, 2, 5, 6, 4, 2};
+
+    int size1 = sizeof(testSize1);
+
+    cout << size1;
+
 
     return 0;
 }
