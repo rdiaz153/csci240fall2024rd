37daec17818880f0689d16e8a2244e2a5e824145
Ricardo Diaz <rdiaz153@student.mtsac.edu>
2024-08-27 21:29:40 -0700

    created exercise2PA1

---
+++ exercise2PA1.cpp
@@ -0,0 +1,18 @@
+/*  Program:    PA 1 Exercise 2
+    Author:     Ricardo Diaz
+    Class:      CSCI 240 
+    Date:       8/22/24
+    Description: Review Basic C++/Java & OOP.
+    I certify that the code below is my own work.
+    Exception(s): N/A
+*/
+#include <iostream>
+
+using namespace std;
+
+int main() 
+{
+  
+
+  return 0;
+}
