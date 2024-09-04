8ad8ec63081218881e09bfa53a195c41d4d2528a
rdiaz153 <rdiaz153@student.mtsac.edu>
2024-09-04 10:08:41 -0700

    9/4

--- exercise3PA1.cpp
+++ exercise3PA1.cpp
@@ -1,6 +1,6 @@
 /*  Program:    PA 1 Exercise 3
     Author:     Ricardo Diaz
-    Class:      CSCI 240 
+    Class:      CSCI 240
     Date:       8/22/24
     Description: Review Basic C++/Java & OOP.
     I certify that the code below is my own work.
@@ -12,46 +12,46 @@
 
 namespace dsac::design {
 
-bool isPrime(int n) {
-  if (n <= 1) {
-    return false;
-  }
+    bool isPrime(int n) {
+        if (n <= 1) {
+            return false;
+        }
 
-  for (int i = 2; i < n; i++){
-    if (n % i == 0) {
-      return false;
-    }
-  }
-  
-  return true;
-}
+        for (int i = 2; i < n; ++i) {
+            if (n % i == 0) {
+                return false;
+            }
+        }
 
-class PrimeProgression : public Progression {
-  protected:
-
-  public: 
-    PrimeProgression() : Progression(2) {}
-
-    PrimeProgression(long start) : Progression(start) {
-      if (!isPrime(start)) {
-        advance();
-      }      
+        return true;
     }
 
+    class PrimeProgression : public Progression {
+    protected:
+        virtual void advance() {
+            do {
+                current++;
+            } while (!isPrime(current));
+        }
 
-};
+    public:
+
+        PrimeProgression(long start = 2) : Progression(start) {}
+
+
+    };
 
 }
 
 int main() {
-  dsac::design::PrimeProgression prog;
-  dsac::design::PrimeProgression primeProg(97);
+    dsac::design::Progression prog;
+    dsac::design::PrimeProgression primeProg(97);
 
-  std::cout << "Normal Progression: ";
-  prog.print_progression(5);
+    std::cout << "Normal Progression: ";
+    prog.print_progression(5);
 
-  std::cout << "Prime Progression starting from 97: ";
-  primeProg.print_progression(5);
+    std::cout << "Prime Progression starting from 97: ";
+    primeProg.print_progression(5);
 
-  return 0;
+    return 0;
 }
