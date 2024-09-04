4fc503544d810ffcc4b2071b4f4b5c5560ff700b
Ricardo Diaz <rdiaz153@student.mtsac.edu>
2024-09-03 23:38:22 -0700

    9/3 finished EC?

--- extracreditPA1.cpp
+++ extracreditPA1.cpp
@@ -13,6 +13,34 @@
 #include <ctime>
 
 using namespace std;
+// create random index(?)
+//if-else or switch?
+//remove or insert or replace a char
+
+string createMistake(string& sentence){
+  string result = sentence;
+  int len = sentence.length();
+  int mistake_type = rand() % 3;
+
+  switch (mistake_type) {
+    case 0: {
+      int pos = rand() % len;
+      result[pos] = 'a' + rand() % 26;
+      break;
+    }
+    case 1: {
+      int pos = rand() % len;
+      result.erase(pos, 1);
+      break;
+    }
+    case 2: {
+      int pos = rand() % len;
+      result.insert(pos, 1, 'a' + rand() % 26);
+      break;
+    }
+  }
+  return result;
+}
 
 int main() {
 
@@ -20,7 +48,17 @@ int main() {
 
   string sentence = "I will always use object-oriented design.";
 
-  
+  for (int i = 1; i <= 50; i++) {
+    string output = sentence;
+    int numMistakes = 0;
+
+    while (numMistakes < 8) {
+      output = createMistake(output);
+      num_mistakes++;
+    }
+
+    cout << i << ": " << output << std::endl;
+  }
 
   return 0;
 }
