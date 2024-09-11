83333d94ca59bc35c3831cde8ed2cf4eff0ca457
rdiaz153 <rdiaz153@student.mtsac.edu>
2024-09-11 09:35:35 -0700

    9/11

---
+++ PA2/Ex3PA2.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+
+int numElems = 0;
+string names[10];
+
+void insertRear(string n);
+
+int main() 
+{
+    insertRear("Jo");
+    
+
+    return 0;
+}
+
+void insertRear(string n)
+{
+
+}
